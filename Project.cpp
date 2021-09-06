ec24078b159dd16057bfcd3c1573c7be21668284
Omar <90169655+OmarHachicho@users.noreply.github.com>
2021-09-06 07:24:02 +0300

    Add files via upload

---
+++ Project.cpp
@@ -0,0 +1,20 @@
+// Developed APP By Omar Hachicho (c) 
+// One of my first CPP Projects
+
+#include <iostream>
+
+
+int main()
+{
+	std::cout << "Enter an integer: ";
+
+	int num{ };
+	std::cin >> num;
+
+	std::cout << "Double that number is: " <<  num * 2 << '\n'; // use an expression to multiply num * 2 at the point where we are going to print it
+
+	return 0;
+}
+
+// Suggest more things for me to do for you to learn CPP!
+// Email: omarhachichogithub@gmail.com
